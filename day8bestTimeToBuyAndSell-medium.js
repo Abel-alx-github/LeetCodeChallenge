ec3ff9bb34c73f6a5ce745d5e5e542b82d4d9d9b
Abel Tibeso <abeltibeso323@gmail.com>
2024-10-04 12:02:08 +0300

    day 8: Best time to buy and sell stock II

--- day8bestTimeToBuyAndSell-medium.js
+++ day8bestTimeToBuyAndSell-medium.js
@@ -51,3 +51,16 @@ This captures all upward movements, allowing for multiple buy-sell transactions,
 maximizing the total profit. The final profit is returned after checking all prices.
 
 */
+
+
+//============== solution 2
+
+var maxProfit = function(prices) {
+     let profit = 0;
+     for(let i = 1; i < prices.length; i++){
+        if(prices[i] > prices[i - 1]) {
+            profit += prices[i] - prices[i - 1]
+            }
+        }
+    return profit
+}
